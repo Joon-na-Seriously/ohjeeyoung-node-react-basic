250dd4c037ece2f1b3ccd1971966c8a957d56b3a
ohjeeyoung <jeeyoung990725@naver.com>
2021-07-19 21:19:43 +0900

    feat:add proxy server, concurrently module

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, {userEffect}  from 'react'
+import axios from 'axios'
 
 function LandingPage() {
+
+    userEffect(() => {
+        axios.get('/api/hello')
+        .then(response => console.log(response.data))
+    }, [])
+
     return (
         <div>
             LandingPage
--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 5000
+
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const config = require('./config/key');
@@ -23,6 +23,11 @@ mongoose.connect(config.mongoURI, {
 
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/api/hello', (req, res) => {
+    res.send("안녕하세요")
+})
+
 
 app.post('/api/users/register', (req, res) => {
     // 회원가입 할 때 필요한 정보들을 client에서 가져오면
@@ -93,5 +98,6 @@ app.get('/api/users/logout', auth, (req, res) =>{
         })
 })
 
+const port = 5000
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
